a87c4656298358a4ca35c794cfef4df692286618
chaipa <chaitanya.patel@flemingcollege.ca>
2020-08-19 21:45:48 -0400

    light level changing with useReducer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useReducer } from 'react';
+
 import './App.css';
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'advance':
+      return (state + 1) %4 ;
+    case 'off':
+      return 0;
+    default:
+      return state;
+
+  }
+}
+
 function App() {
+  const [lightLevel, dispatch] = useReducer(reducer, 0)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>LIghts are:{describeLightLevel(lightLevel)}</h1>
+
+      <button
+        onClick={() => dispatch({ type: 'advance' })}
+      >Change the level</button>
+      <button
+        onClick={() => dispatch({ type: 'off' })}
+      >Off</button>
+
     </div>
   );
+}
+
+function describeLightLevel(level) {
+  switch (level) {
+    case 0:
+      return 'off';
+    case 1:
+      return 'low';
+    case 2:
+      return 'medium';
+    case 3:
+      return 'high';
+    default:
+      return 'broken '
+
+  }
 }
 
 export default App;
